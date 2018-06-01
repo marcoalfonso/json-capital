6bca91db8dff759fb77e9a602a39f54056a2fa81
Marco Lavielle <marcoalfonso@gmail.com>
2018-06-01 11:18:55 +1000

    Update truffle config

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # JSON CAPITAL
 
 JSON CAPITAL has built an unrivaled network of entrepreneurs, advisors, and limited partners who are on the front lines of this fast moving sector of Fintech. The principals of the firm believe that Blockchain technology is a profound invention that offers a better, faster, cheaper way to move money and exchange assets without counterparty risk.
+
+### To run application
+
+To boot up the basic environment run `npm start`. Navigate to [localhost:3000](http://localhost:3000)
--- truffle.js
+++ truffle.js
@@ -1,17 +1,44 @@
-require('dotenv').config();
-const Web3 = require("web3");
-const web3 = new Web3();
-const WalletProvider = require("truffle-wallet-provider");
-const Wallet = require('ethereumjs-wallet');
-
-var mainNetPrivateKey = new Buffer(process.env["MAINNET_PRIVATE_KEY"], "hex")
-var mainNetWallet = Wallet.fromPrivateKey(mainNetPrivateKey);
-var mainNetProvider = new WalletProvider(mainNetWallet, "https://mainnet.infura.io/");
-
-var ropstenPrivateKey = new Buffer(process.env["ROPSTEN_PRIVATE_KEY"], "hex")
-var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
-var ropstenProvider = new WalletProvider(ropstenWallet, "https://ropsten.infura.io/");
-
+// require('dotenv').config();
+// const Web3 = require("web3");
+// const web3 = new Web3();
+// const WalletProvider = require("truffle-wallet-provider");
+// const Wallet = require('ethereumjs-wallet');
+//
+// var mainNetPrivateKey = new Buffer(process.env["MAINNET_PRIVATE_KEY"], "hex")
+// var mainNetWallet = Wallet.fromPrivateKey(mainNetPrivateKey);
+// var mainNetProvider = new WalletProvider(mainNetWallet, "https://mainnet.infura.io/");
+//
+// var ropstenPrivateKey = new Buffer(process.env["ROPSTEN_PRIVATE_KEY"], "hex")
+// var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
+// var ropstenProvider = new WalletProvider(ropstenWallet, "https://ropsten.infura.io/");
+//
+//
+// module.exports = {
+//   migrations_directory: "./migrations",
+//   networks: {
+//     development: {
+//       host: "localhost",
+//       port: 8545,
+//       network_id: "*"
+//     },
+//     ropsten: {
+//       provider: ropstenProvider,
+//       // You can get the current gasLimit by running
+//       // truffle deploy --network rinkeby
+//       // truffle(rinkeby)> web3.eth.getBlock("pending", (error, result) =>
+//       //   console.log(result.gasLimit))
+//       gas: 4700000,
+//       gasPrice: web3.toWei("20", "gwei"),
+//       network_id: "3",
+//     },
+//     mainnet: {
+//       provider: mainNetProvider,
+//       gas: 7990000,
+//       gasPrice: web3.toWei("20", "gwei"),
+//       network_id: "1",
+//     }
+//   }
+// };
 
 module.exports = {
   migrations_directory: "./migrations",
@@ -20,33 +47,6 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*"
-    },
-    ropsten: {
-      provider: ropstenProvider,
-      // You can get the current gasLimit by running
-      // truffle deploy --network rinkeby
-      // truffle(rinkeby)> web3.eth.getBlock("pending", (error, result) =>
-      //   console.log(result.gasLimit))
-      gas: 4700000,
-      gasPrice: web3.toWei("20", "gwei"),
-      network_id: "3",
-    },
-    mainnet: {
-      provider: mainNetProvider,
-      gas: 7990000,
-      gasPrice: web3.toWei("20", "gwei"),
-      network_id: "1",
     }
   }
 };
-
-// module.exports = {
-//   migrations_directory: "./migrations",
-//   networks: {
-//     development: {
-//       host: "localhost",
-//       port: 8545,
-//       network_id: "*"
-//     }
-//   }
-// };
